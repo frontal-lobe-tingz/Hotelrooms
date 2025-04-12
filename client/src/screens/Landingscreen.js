fa383fcb395c014db271bdd776c2b22592f0ccf5
frontal-lobe-tingz <ndumisotheodore@gmail.com>
2025-04-12 04:05:56 +0200

    Update Landingscreen.js

--- client/src/screens/Landingscreen.js
+++ client/src/screens/Landingscreen.js
@@ -7,7 +7,7 @@ function Landingscreen() {
       <div className="col-md-9 my-auto text-center" style={{ borderRight: '8px solid white' }}>
 
         <h2 style={{ color: 'white', fontSize: '100px' }}>Hotel rooms</h2>
-        <h1 style={{ color: 'white' }}>Book your meeting</h1>
+        <h1 style={{ color: 'white' }}>Book your room</h1>
 
         <Link to='/home'>
           <button className='btn landingbtn' style={{ color: 'black', backgroundColor: 'white' }}>Getting started</button>
